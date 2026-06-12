dfd50c2100da5ffb68449377acb87060ae17bd75
Abdul Moeed <amoeed299@gmail.com>
2026-06-12 22:36:35 +0200

    Upload PINNs WAAM repository files with Git LFS

---
+++ master-thesis/scripts/surrogate_model_training_v1.2(PINNs)_S.Hartmann_split-latest.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+
+nohup python3 surrogate_model_training_v1.2\(PINNs\)_S.Hartmann_split-latest.py > ../logs/surrogate_model_training_v1.2\(PINNs\)_S.Hartmann_split-latest.log 2>&1 &
