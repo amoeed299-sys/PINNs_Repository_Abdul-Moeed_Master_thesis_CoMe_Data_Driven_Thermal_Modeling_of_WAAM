f7531fcfcdc92a932a9f52d184f7149a86431132
Abdul Moeed <amoeed299@gmail.com>
2026-06-24 21:14:32 +0200

    Update surrogate_model_training_v1.2(PINNs)-latest.sh

--- master-thesis/scripts/surrogate_model_training_v1.2(PINNs)-latest.sh
+++ master-thesis/scripts/surrogate_model_training_v1.2(PINNs)-latest.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-nohup python3 surrogate_model_training_v1.2\(PINNs\)_S.Hartmann_split-latest.py > ../logs/surrogate_model_training_v1.2\(PINNs\)_S.Hartmann_split-latest.log 2>&1 &
+nohup python3 surrogate_model_training_v1.2\(PINNs\)-latest.py > ../logs/surrogate_model_training_v1.2\(PINNs\)-latest.log 2>&1 &
